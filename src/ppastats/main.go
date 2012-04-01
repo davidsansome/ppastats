3191ddb8cb1f2d0e7451b01fbc00257ffa0fa7dc
David Sansome <me@davidsansome.com>
2012-04-02 00:11:07 +0100

    Commandline flags

--- src/ppastats/main.go
+++ src/ppastats/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
   "fmt"
+  "flag"
   "launchpad"
+  "os"
   "time"
 )
 
 func main() {
+  repo    := flag.String("repo", "", "Launchpad repository name")
+  archive := flag.String("archive", "", "Launchpad archive name")
+  flag.Parse()
+
+  if *repo == "" || *archive == "" {
+    fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "-repo <repo> -archive <archive>")
+    return
+  }
+
   a := launchpad.Archive{
-    Repository: "~me-davidsansome",
-    Id: "clementine",
+    Repository: *repo,
+    Id:         *archive,
   }
 
   if ret, err := a.GetPublishedBinaries(); err != nil {
